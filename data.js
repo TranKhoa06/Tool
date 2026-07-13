9e81191e14a786cd3a91664d29cd27e14a876c4d
TranKhoa06 <takhoa2017@gmail.com>
2026-07-13 13:17:36 +0700

    Initial commit of tool

---
+++ data.js
@@ -0,0 +1,75 @@
+const EXAM_LIST = [
+        {
+            "name": "de_acc302_FA23",
+            "folderDe": "de_acc302_FA23",
+            "folderDapan": "dapan_acc302_FA23",
+            "folderLoigiai": null
+        },
+        {
+            "name": "de_acc302_FA24",
+            "folderDe": "de_acc302_FA24",
+            "folderDapan": "dapan_acc302_FA24",
+            "folderLoigiai": null
+        },
+        {
+            "name": "de_acc302_SP25",
+            "folderDe": "de_acc302_SP25",
+            "folderDapan": "dapan_acc302_SP25",
+            "folderLoigiai": null
+        },
+        {
+            "name": "de_acc302_SU24",
+            "folderDe": "de_acc302_SU24",
+            "folderDapan": "dapan_acc302_SU24",
+            "folderLoigiai": null
+        },
+        {
+            "name": "de_fin201_FA24",
+            "folderDe": "de_fin201_FA24",
+            "folderDapan": "dapan_fin201_FA24",
+            "folderLoigiai": null
+        },
+        {
+            "name": "de_fin201_SP25",
+            "folderDe": "de_fin201_SP25",
+            "folderDapan": "dapan_fin201_SP25",
+            "folderLoigiai": null
+        },
+        {
+            "name": "de_fin201_SU24",
+            "folderDe": "de_fin201_SU24",
+            "folderDapan": "dapan_fin201_SU24",
+            "folderLoigiai": null
+        },
+        {
+            "name": "de_fin303_1",
+            "folderDe": "de_fin303_1",
+            "folderDapan": "dapan_fin303_1",
+            "folderLoigiai": null
+        },
+        {
+            "name": "de_fin303_2",
+            "folderDe": "de_fin303_2",
+            "folderDapan": "dapan_fin303_2",
+            "folderLoigiai": null
+        },
+        {
+            "name": "de_fin303_3",
+            "folderDe": "de_fin303_3",
+            "folderDapan": "dapan_fin303_3",
+            "folderLoigiai": null
+        },
+        {
+            "name": "de_fin303_4",
+            "folderDe": "de_fin303_4",
+            "folderDapan": "dapan_fin303_4",
+            "folderLoigiai": null
+        },
+        {
+            "name": "de_fin303_5",
+            "folderDe": "de_fin303_5",
+            "folderDapan": "dapan_fin303_5",
+            "folderLoigiai": null
+        }
+        
+];
